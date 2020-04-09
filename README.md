a481f79b38e444b76e1e02385dae53cd73760081
David Morrin <dm187@nyu.edu>
2020-04-09 11:05:46 -0400

    feat: basic backend for react app testing

---
+++ README.md
@@ -0,0 +1,9 @@
+# create react app backend
+
+Something to serve MySQL data for my local create react app projects.
+
+Followed along with [this article](https://daveceddia.com/create-react-app-express-backend/).
+
+Database details should be stored in a `.env` file.
+
+Run with `npm start`.
---
+++ routes/events.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: process.env.REACT_APP_DATA_HOST,
+    user: process.env.REACT_APP_DATA_USER,
+    password: process.env.REACT_APP_DATA_PASSWORD,
+    database: process.env.REACT_APP_DATA_DATABASE
+  })
+
+  connection.connect();
+  connection.query(process.env.REACT_APP_DATA_QUERY, function (err, rows, fields) {
+    if (err) res.json(err)
+    res.json(rows);
+  })
+});
+
+module.exports = router;
